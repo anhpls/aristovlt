910cdc095f146f444e8c86d1d661a43633e94a1e
anhpls <anh.h.duy@gmail.com>
2025-01-04 08:10:46 -0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="../images/favicon.png" />
+      </head>
       <body className={`${inter.className} antialiased`}>
         <CartProvider>
           <CartDrawer />
