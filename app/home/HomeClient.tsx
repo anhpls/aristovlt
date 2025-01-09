6cc6282e6c659a2f720854496cdef6e513a7d866
anhpls <anh.h.duy@gmail.com>
2025-01-08 17:07:21 -0800

    fix cart on homepage

--- app/home/HomeClient.tsx
+++ app/home/HomeClient.tsx
@@ -6,23 +6,21 @@ import { memo } from "react";
 
 const HomeClient = () => {
   return (
-    <>
-      <HeaderWithNavBar />
-      <div className="relative h-screen w-screen">
-        <div className="absolute inset-0 -z-10">
-          <Image
-            src="/images/homebg2.png"
-            alt="Home Background"
-            fill
-            style={{
-              objectFit: "cover",
-              objectPosition: "top",
-            }}
-            priority
-          />
-        </div>
+    <div className="relative h-screen w-screen">
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src="/images/homebg2.png"
+          alt="Home Background"
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "top",
+          }}
+          priority
+        />
       </div>
-    </>
+      <HeaderWithNavBar />
+    </div>
   );
 };
 
